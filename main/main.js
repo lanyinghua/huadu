872e97a2fcde3be985cabd636979decf98ee9572
lanyinghua <448786703@qq.com>
2017-06-23 14:46:04 +0800

    main.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,29 @@
 module.exports = function main() {
     console.log("Debug Info");
     return 'Hello World!';
+	
 };
+
+	{
+	var x;
+	var y;
+	var result;
+	scarf("%d",x);
+	scarf("%d",y);
+	
+	if (!isNaN(x))
+	{
+		if(!isNaN(y))
+		{	console.log (result=x%y )}
+			else{
+				console.log("输入错误")}
+	
+	}
+	else {console.log("输入错误")}
+	
+}
+	
+	
+	
+
+ 
