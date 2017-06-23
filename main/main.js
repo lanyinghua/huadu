a6a1ffc0425c82c444ae9885ae6499db7305cf8f
lanyinghua <448786703@qq.com>
2017-06-23 19:01:37 +0800

    add main.js

--- main/main.js
+++ main/main.js
@@ -1,11 +1,11 @@
 module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+   
 	var x=9;
 	var y=4;
 	var result;
-	
-	console.log (result=x%y )
+	console.log (result=x%y );
+	  console.log("Debug Info");
+    return 'Hello World!';
 };
 
 
